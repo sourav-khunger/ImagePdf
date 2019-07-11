626aa28811a9af0275ba4ded4dab117c05192109
sourav-khunger <khungersourav30@gmail.com>
2019-07-11 17:43:57 +0530

    new activities

--- app/src/main/java/com/example/imagepdf/CreatePdfActivity.java
+++ app/src/main/java/com/example/imagepdf/CreatePdfActivity.java
@@ -3,56 +3,105 @@ package com.example.imagepdf;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+
+import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 
 public class CreatePdfActivity extends AppCompatActivity {
     ImageView iv_photo;
     Button btn_createpdf;
+    Dialog dialog;
+    EditText et_save_pdf_file;
+    Button save_pdf_btn;
+    String pdf;
+    String dirpath;
+    Bitmap bitmap;
+    private static String FILE = "";
+    static Image image;
+    static byte[] bArray;
+    String p;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_pdf);
-        iv_photo=findViewById(R.id.iv_photo);
-        btn_createpdf=findViewById(R.id.btn_createpdf);
+        iv_photo = findViewById(R.id.iv_photo);
+        btn_createpdf = findViewById(R.id.btn_createpdf);
+        p = Environment.getExternalStorageDirectory() + "/AVI PDF FORMS/";
+        File path = new File(p);
+        if (!path.exists()) {
+            path.mkdirs();
+        }
 
         selectImage();
+
+        iv_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
+        btn_createpdf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+                //imageToPDF();
+            }
+        });
+
+
     }
 
+    //dialog for image chooser
     private void selectImage() {
-
-        final CharSequence[] options = { "Take Photo", "Choose from Gallery"};
+        final CharSequence[] options = {"Take Photo", "Choose from Gallery"};
         AlertDialog.Builder builder = new AlertDialog.Builder(CreatePdfActivity.this);
         builder.setTitle("Add Photo!");
         builder.setItems(options, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int item) {
-                if (options[item].equals("Take Photo"))
-                {
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
+                if (options[item].equals("Take Photo")) {
+
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
                     intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
                     startActivityForResult(intent, 1);
-                }
-                else if (options[item].equals("Choose from Gallery"))
-                {
-                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                } else if (options[item].equals("Choose from Gallery")) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                     startActivityForResult(intent, 2);
                 }
             }
@@ -73,21 +122,19 @@ public class CreatePdfActivity extends AppCompatActivity {
                     }
                 }
                 try {
-                    Bitmap bitmap;
                     BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
-                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
-                            bitmapOptions);
+                    bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    bArray = stream.toByteArray();
                     iv_photo.setImageBitmap(bitmap);
-                    String path = android.os.Environment
-                            .getExternalStorageDirectory()
-                            + File.separator
-                            + "Phoenix" + File.separator + "default";
+
+                    String path = android.os.Environment.getExternalStorageDirectory() + File.separator + "Phoenix" + File.separator + "default";
                     f.delete();
                     OutputStream outFile = null;
                     File file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg");
                     try {
                         outFile = new FileOutputStream(file);
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 85, outFile);
                         outFile.flush();
                         outFile.close();
                     } catch (FileNotFoundException e) {
@@ -102,16 +149,113 @@ public class CreatePdfActivity extends AppCompatActivity {
                 }
             } else if (requestCode == 2) {
                 Uri selectedImage = data.getData();
-                String[] filePath = { MediaStore.Images.Media.DATA };
-                Cursor c = getContentResolver().query(selectedImage,filePath, null, null, null);
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath, null, null, null);
                 c.moveToFirst();
                 int columnIndex = c.getColumnIndex(filePath[0]);
                 String picturePath = c.getString(columnIndex);
                 c.close();
-                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
-                //Log.w("path of image from gallery......******************.........", picturePath+"");
-                iv_photo.setImageBitmap(thumbnail);
+                bitmap = (BitmapFactory.decodeFile(picturePath));
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                bArray = stream.toByteArray();
+
+
+                iv_photo.setImageBitmap(bitmap);
             }
         }
     }
+
+    //show dialog on create pdf
+    void showDialog() {
+        dialog = new Dialog(this);
+        dialog.setContentView(R.layout.save_as_dialog);
+        dialog.show();
+        et_save_pdf_file = dialog.findViewById(R.id.et_pdf_file_name);
+        save_pdf_btn = dialog.findViewById(R.id.pdf_gen_btn);
+
+        save_pdf_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (et_save_pdf_file.getText().toString().equals("")) {
+                    et_save_pdf_file.setError("Please Enter Name");
+                } else {
+                    pdf = et_save_pdf_file.getText().toString();
+                    try {
+                        imageToPDF(pdf);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    Log.e("PDF", "PDF GENERATED!");
+                }
+
+            }
+        });
+    }
+
+    public void imageToPDF(String pdf) throws FileNotFoundException {
+       /* try {
+           Document document = new Document();
+            dirpath = Environment.getExternalStorageDirectory() + "/AVI PDF FORMS/";
+            PdfWriter.getInstance(document, new FileOutputStream(dirpath + "/" + pdfFileName + ".pdf")); //  Change pdf's name.
+            document.open();
+            Image img = Image.getInstance(Environment.getExternalStorageDirectory() + "/AVI PDF FORMS/" + File.separator + "image.png");
+            float scaler = ((document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin() - 100) / img.getWidth()) * 100;
+            img.scaleToFit(PageSize.A4.getWidth(), PageSize.A4.getHeight());
+            float x = (PageSize.A4.getWidth() - img.getScaledWidth()) / 2;
+            float y = (PageSize.A4.getHeight() - img.getScaledHeight()) / 2;
+            img.setAbsolutePosition(x, y);
+            document.add(img);
+            document.close();
+            Toast.makeText(this, "PDF Generated successfully!..", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+        } catch (Exception e) {
+
+        }*/
+
+        try {
+            Document document = new Document();
+            //PdfWriter.getInstance(document, new FileOutputStream(dirpath + "/" + "Hello" + ".pdf")); //  Change pdf's name.
+            PdfWriter.getInstance(document, new FileOutputStream(p + "/" + pdf + ".pdf")); //  Change pdf's name.
+            document.open();
+            addImage(document);
+            document.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addImage(Document document) {
+        try {
+            image = Image.getInstance(bArray);
+            //image = Image.getInstance(Environment.getExternalStorageDirectory() + "/AVI PDF FORMS/");
+            float scaler = ((document.getPageSize().getWidth() - document.leftMargin() - document.rightMargin() - 100) / image.getWidth()) * 100;
+            image.scaleToFit(PageSize.A4.getWidth(), PageSize.A4.getHeight());
+            float x = (PageSize.A4.getWidth() - image.getScaledWidth()) / 2;
+            float y = (PageSize.A4.getHeight() - image.getScaledHeight()) / 2;
+            image.setAbsolutePosition(x, y);
+            document.add(image);
+            document.close();
+            Toast.makeText(this, "PDF generated successfully", Toast.LENGTH_SHORT).show();///Here i set byte array..you can do bitmap to byte array and set in image...
+            dialog.dismiss();
+        } catch (BadElementException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        // image.scaleAbsolute(150f, 150f);
+        try {
+            document.add(image);
+        } catch (DocumentException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/imagepdf/MainActivity.java
+++ app/src/main/java/com/example/imagepdf/MainActivity.java
@@ -1,27 +1,95 @@
 package com.example.imagepdf;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.viewpagerindicator.CirclePageIndicator;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 LinearLayout ll_createpdf,ll_mergepdf,ll_savedpdf;
+    List<String> imagePathList=new ArrayList<>();
+    List<String> imagesEncodedList=new ArrayList<>();
+    String imageEncoded;
+    Uri mImageUri;
+
+    EditText et_save_pdf_file;
+    Button save_pdf_btn;
+    Dialog dialog;
+    static Image image;
+    String p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        p= Environment.getExternalStorageDirectory() + "/AVI PDF FORMS/";
+        File path = new File(p);
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED ) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.CAMERA) && ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) && ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+
+
+            }}
         /*getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
@@ -46,10 +114,173 @@ LinearLayout ll_createpdf,ll_mergepdf,ll_savedpdf;
                 break;
 
             case R.id.ll_mergepdf:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
+
                 break;
 
             case R.id.ll_savedpdf:
+                startActivity(new Intent(MainActivity.this,PdfFilesActivity.class));
+
                 break;
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        try {
+            // When an Image is picked
+            if (requestCode == 1 && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                imagesEncodedList = new ArrayList<String>();
+                if (data.getData() != null) {
+
+                    mImageUri = data.getData();
+                    imagePathList.add(getImagePath(mImageUri));
+                    // Get the cursor
+                    Cursor cursor = getContentResolver().query(mImageUri,
+                            filePathColumn, null, null, null);
+                    // Move to first row
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    imageEncoded = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    showSavePdfDialog(false);
+                    Log.v("Image Path SINGLE", imagePathList.get(0));
+
+                } else {
+                    if (data.getClipData() != null) {
+                        ClipData mClipData = data.getClipData();
+                        ArrayList<Uri> mArrayUri = new ArrayList<Uri>();
+                        for (int i = 0; i < mClipData.getItemCount(); i++) {
+
+                            ClipData.Item item = mClipData.getItemAt(i);
+                            Uri uri = item.getUri();
+                            mArrayUri.add(uri);
+                            imagePathList.add(getImagePath(uri));
+                            // Get the cursor
+                            Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                            // Move to first row
+                            cursor.moveToFirst();
+
+                            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                            imageEncoded = cursor.getString(columnIndex);
+                            imagesEncodedList.add(imageEncoded);
+                            cursor.close();
+
+                        }
+                        Log.v("LOG_TAG", "Selected Images" + mArrayUri.size() + mArrayUri.get(0));
+                        Log.v("Image Path", getImagePath(mArrayUri.get(0)));
+                        showSavePdfDialog(true);
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong" + e, Toast.LENGTH_LONG)
+                    .show();
+            Log.v("Exception", e + "");
+
+        }
+    }
+
+    void showSavePdfDialog(final boolean more) {
+        dialog=new Dialog(this);
+        dialog.setContentView(R.layout.save_pdf_dialog);
+        dialog.show();
+        final EditText pdf_name = dialog.findViewById(R.id.pdf_name);
+        Button merge_files = dialog.findViewById(R.id.save_pdf);
+
+        merge_files.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pdf_name.getText().toString().equals("")) {
+                    pdf_name.setError("Enter Pdf Name");
+                } else {
+                    try {
+                        if (more) {
+                            createPdf(pdf_name.getText().toString());
+                        } else {
+                            createPdfSingleImage(pdf_name.getText().toString());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (DocumentException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        });
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialogInterface) {
+                imagePathList.clear();
+            }
+        });
+
+    }
+
+    public String getImagePath(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+
+        return path;
+    }
+
+    public void createPdf(String pdfFileName) throws IOException, DocumentException {
+        Image img = Image.getInstance(imagePathList.get(0));
+        Document document = new Document(img);
+        PdfWriter.getInstance(document, new FileOutputStream(p + "/" + pdfFileName + ".pdf"));
+        document.open();
+        for (String image : imagePathList) {
+            img = Image.getInstance(image);
+            document.setPageSize(img);
+            document.newPage();
+            img.setAbsolutePosition(0, 0);
+            document.add(img);
+        }
+        document.close();
+        Toast.makeText(this, "PDF Saved!", Toast.LENGTH_SHORT).show();
+        dialog.dismiss();
+    }
+
+    public void createPdfSingleImage(String pdfFileName) throws IOException, DocumentException {
+        Image img = Image.getInstance(getImagePath(mImageUri));
+        Document document = new Document(img);
+        PdfWriter.getInstance(document, new FileOutputStream(p + "/" + pdfFileName + ".pdf"));
+        document.open();
+        img = Image.getInstance(getImagePath(mImageUri));
+        document.setPageSize(img);
+        document.newPage();
+        img.setAbsolutePosition(0, 0);
+        document.add(img);
+
+        document.close();
+        dialog.dismiss();
+        Toast.makeText(this, "PDF Saved!", Toast.LENGTH_SHORT).show();
     }
 }
